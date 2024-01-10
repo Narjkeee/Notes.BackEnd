94c7b5eec9177fbc6dfa0edd309ede20d9a16bb4
Narek Petrosyan <narek.petrosyan.a@gmail.com>
2024-01-11 02:04:20 +0400

    Add project files.

---
+++ Notes.Persistance/NoteDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistance.EntityTypeConfigurations;
+
+namespace Notes.Persistance
+{
+    public class NoteDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NoteDbContext(DbContextOptions<NoteDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
